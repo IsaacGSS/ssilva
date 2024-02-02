0674672b483523048e138903e9d0b997a8fd3da7
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-02-02 20:32:59 -0300

    add templet

---
+++ app/components/project/Project.tsx
@@ -0,0 +1,16 @@
+export const Projetc = () => {
+  return (
+    <div className='max-w-screen-lg m-auto mb-10 grid gap-5 grid-cols-3 grid-rows-4 grid-flow-row-dense'>
+      <div className='bg-emerald-500 size-64 rounded-lg w-full bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />
+      <div className='bg-emerald-500 size-64 rounded-lg w-full col-span-2 bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />{' '}
+      {/* <div className='bg-emerald-500 size-64 rounded-lg' /> */}
+      <div className='bg-emerald-500 size-64 rounded-lg w-full col-span-2 bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />{' '}
+      <div className='bg-emerald-500 size-64 rounded-lg w-full bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />{' '}
+      {/* <div className='bg-emerald-500 size-64 rounded-lg' /> */}
+      <div className='bg-emerald-500 size-64 rounded-lg w-full h-full row-span-2 bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />{' '}
+      <div className='bg-emerald-500 size-64 rounded-lg w-full col-span-2 bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />{' '}
+      <div className='bg-emerald-500 size-64 rounded-lg w-full h-full bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />
+      <div className='bg-emerald-500 size-64 rounded-lg w-full h-full bg-cover bg-center bg-no-repeat bg-[url(https://github.com/IsaacGSS.png)]' />
+    </div>
+  )
+}
