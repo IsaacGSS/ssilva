8c8fdb479ada4dcb9a92ecf0f20a9b0213ab525a
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-02-04 20:32:53 -0300

    melhorar da legibilidade
    uma mudanca feita para melhora da legibilidade e deenvolvimenento mais limpo no design

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,17 @@ export default function Home() {
   return (
     <>
       <main className='min-h-screen -mt-16 flex flex-col text-white justify-center items-center gap-5'>
-        <p className='text-sm font-light font-lato text-white/95 tracking-widest text-wrap text-center '>
-          Ola, Me chamo Isaac S. Silva
-        </p>
-        <h1 className=' text-7xl md:text-8xl lg:text-9xl font-sansilk bg-gradient-to-r from-[#800056] to-[#be3455] to-60% bg-clip-text  tracking-tighter text-transparent'>
+        <h1 className=' select-none text-7xl md:text-8xl lg:text-9xl font-sansilk bg-gradient-to-r from-[#800056] to-[#be3455] to-60% bg-clip-text  tracking-tighter text-transparent'>
           S. Silva
         </h1>
-        <h2 className='text-sm font-light font-lato text-white/95 tracking-widest lg:w-1/2 md:w-[80%] w-full px-5 text-wrap text-center'>
-          sou {''}
-          <b className='font-semibold text-white/80'>Desenvolvedor Web</b>. Crio
-          e desenvolvo solucoes com criatividade para suas paginas web e outras
-          aplicacoes para sua nessecidade.
-        </h2>
+        <p className='text-sm font-lato text-zinc-50/70 tracking-widest leading-7 lg:w-1/2 md:w-[80%] w-full px-5 text-wrap text-center'>
+          Ola, Me chamo{' '}
+          <b className='font-semibold text-zinc-50'>Isaac S. Silva</b> sou {''}
+          <b className='font-semibold text-zinc-50'>Desenvolvedor Web</b>.
+          <br />
+          Crio e desenvolvo solucoes com criatividade para suas paginas web e
+          outras aplicacoes para sua nessecidade.
+        </p>
         <br />
       </main>
 
