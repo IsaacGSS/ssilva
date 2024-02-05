da7a73068fe9981c788ddce816082f9641e79436
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-02-05 00:51:04 -0300

    ,

--- app/components/command/CommandUltra.tsx
+++ app/components/command/CommandUltra.tsx
@@ -23,7 +23,10 @@ export function CommandUltra() {
   return (
     <>
       <Tag>
-        <div onClick={() => setOpen(open => !open)}>
+        <div
+          className='bg-gradient-to-r from-[#800056] to-[#be3455] to-60% rounded w-6 h-6 flex justify-center items-center cursor-pointer'
+          onClick={() => setOpen(open => !open)}
+        >
           <MoonStar className=' m-auto fill-zinc-100/80 text-zinc-100/80 size-4' />
         </div>
       </Tag>
--- app/components/command/Tag.tsx
+++ app/components/command/Tag.tsx
@@ -1,11 +1,7 @@
-import { ReactNode } from 'react'
-
-export const Tag = (children: ReactNode) => {
+export const Tag = (children: any) => {
   return (
     <div className='flex flex-row gap-2 md:static absolute bg-center md:translate-x-0 translate-x-24'>
-      <div className='bg-gradient-to-r from-[#800056] to-[#be3455] to-60% rounded w-6 h-6 flex justify-center items-center cursor-pointer'>
-        {children}
-      </div>
+      {children}
 
       <p className='text-sm  text-muted-foreground tracking-wider translate-y-0.5 md:block hidden'>
         Press{' '}
