1c202075534734c165a34811c3b936b5ced79e19
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-13 03:06:37 -0300

    Update Tag.tsx

--- app/components/command/Tag.tsx
+++ app/components/command/Tag.tsx
@@ -9,10 +9,10 @@ export const Tag = ({ children }: itag) => {
     <div className='flex flex-row gap-2 md:static absolute bg-center md:translate-x-0 translate-x-24'>
       {children}
 
-      <p className='text-sm  text-muted-foreground tracking-wider translate-y-0.5 md:block hidden'>
+      <p className='text-sm text-muted-foreground tracking-wider translate-y-0.5 md:block hidden'>
         Press{' '}
         <kbd className='p-1 inline-flex items-center pointer-events-none select-none bg-zinc-500/35 rounded h-5 text-xs font-mono text-zinc-100/80'>
-          ctrl &apos;/&apos;
+          &apos;/&apos;
         </kbd>
       </p>
     </div>
